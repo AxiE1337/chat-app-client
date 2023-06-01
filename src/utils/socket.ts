f36b42358dcfdc123b08f02460ac817ba72abbe6
AxiE1337 <pixell390@gmail.com>
2023-06-01 19:52:56 +0300

    socket updated socket settings

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -2,4 +2,6 @@ import { io } from 'socket.io-client'
 
 const URL = import.meta.env.PROD ? undefined : 'http://localhost:3001'
 
-export const socket = io(URL as string)
+export const socket = io(URL as string, {
+  withCredentials: true,
+})
