a21000bd77636446cfaa6eb27b638b61dd909d22
AxiE1337 <pixell390@gmail.com>
2023-06-01 20:41:16 +0300

    added prod url to client socket

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,6 +1,9 @@
 import { io } from 'socket.io-client'
 
-const URL = import.meta.env.PROD ? undefined : 'http://localhost:3001'
+const URL = import.meta.env.PROD
+  ? import.meta.env.VITE_BASE_FETCH_URL
+  : 'http://localhost:3001'
+console.log(URL)
 
 export const socket = io(URL as string, {
   withCredentials: true,
