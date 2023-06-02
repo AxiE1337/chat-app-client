ce5b8d5f47ba1b0196fa15b2873dc41091111e89
AxiE1337 <pixell390@gmail.com>
2023-06-02 12:58:45 +0300

    added error page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,10 +2,12 @@ import App from './pages/App'
 import SingIn from './pages/SingIn'
 import Layout from './pages/Layout'
 import SignUp from './pages/SignUp'
+import Error from './pages/Error'
 
 const routes = [
   {
     element: <Layout />,
+    errorElement: <Error />,
     children: [
       { path: '/', element: <App /> },
       { path: '/signin', element: <SingIn /> },
