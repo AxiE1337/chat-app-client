21b8f1107d721aab317d08ecaac176fab4a6c0f2
AxiE1337 <pixell390@gmail.com>
2023-06-11 14:29:25 +0300

    addde message to loading

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import { useQuery } from 'react-query'
 import { userAuth } from '../utils/api'
@@ -5,6 +6,7 @@ import SideBar from '../components/SideBar'
 
 const Layout = () => {
   const navigate = useNavigate()
+  const [message, setMessage] = useState<string>('')
   const { pathname } = useLocation()
   const { data, isLoading } = useQuery('auth', userAuth, {
     onSettled(data) {
@@ -26,10 +28,23 @@ const Layout = () => {
     },
   })
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setMessage(
+        'You will probably need to wait for a minute for backend to spin up.'
+      )
+    }, 2000)
+    return () => {
+      setMessage('')
+      clearTimeout(timeout)
+    }
+  }, [])
+
   if (isLoading) {
     return (
       <div className='flex flex-col min-h-screen items-center justify-center'>
         <span className='loading loading-ring loading-lg'></span>
+        <p>{message}</p>
       </div>
     )
   }
